03ea3211952fa811b653afd6e43c3d8876258c0a
Frederico Oliveira <fred@webreakstuff.com>
2008-07-03 16:34:39 +0100

    Added link to create space from space page itself

--- app/models/space.rb
+++ app/models/space.rb
@@ -3,6 +3,7 @@ class Space < ActiveRecord::Base
 	acts_as_tree
 
 	has_many :discussions, :include => [:user], :order => "created_at DESC"
+	#has_many :pages, :order => "created_at DESC"
 	has_many :activities, :as => :reference
 
 	validates_presence_of :name, :alias
