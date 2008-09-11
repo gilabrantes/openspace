cc2ffb2e5b14a9648d4db5ec9232f649ff8e4249
Frederico Oliveira <fred@webreakstuff.com>
2008-09-11 03:38:19 +0100

    Added configuration file and ability to disable signups

--- config/environment.rb
+++ config/environment.rb
@@ -66,3 +66,7 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
 end
+
+
+# loads Openspace configuration
+OPENSPACE_CONFIG = YAML.load_file(RAILS_ROOT + '/config/openspace.yml')
